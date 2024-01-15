0b35ea93731c6ff88ee55cc29187b6ea02d1effd
josaputra15 <josephrama1510@gmail.com>
2024-01-14 23:27:12 -0600

    demo7

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-      <h1 className='text-gradient from-red-500 to-purple-500 text-xl font-bold'>SumAI</h1>
+      <h1 className='cyan_gradient from-red-500 to-purple-500 text-xl font-bold'>SumAI</h1>
 
         <button
           type='button'
@@ -21,7 +21,7 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='red_gradient '>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
         Simplify your reading with SumAI
